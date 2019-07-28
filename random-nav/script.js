7e2dfdf55062647c493e0c6d6e0a5e0468bab8a6
mpqmpqm <53021641+mpqmpqm@users.noreply.github.com>
2019-07-28 16:00:26 -0400

    random-nav v1

---
+++ random-nav/script.js
@@ -0,0 +1,23 @@
+var randButton = document.querySelectorAll('div');
+
+
+console.log(randButton[1]);
+console.log(randButton[0]);
+
+function assignButtons() {
+
+    var decider = Math.floor(Math.random() * 2);
+    console.log(decider);
+    
+    if (decider == 0) {
+        randButton[0].innerHTML = "<a href='#'>MP(Q?)</a>";
+        randButton[1].innerHTML = "<a href='#'>MPQ</a>";
+    }
+    
+    else {
+        randButton[1].innerHTML = "<a href='#'>MP(Q?)</a>";
+        randButton[0].innerHTML = "<a href='#'>MPQ</a>";
+    }
+}
+    
+window.setInterval(assignButtons, 540);
