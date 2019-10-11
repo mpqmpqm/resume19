eea703967464a87e051314c97e8d3bc94b59b04a
mpqmpqm <53021641+mpqmpqm@users.noreply.github.com>
2019-10-10 21:14:16 -0400

    Reconfig display animations
    Faster out than in

--- Tooltip-pet/script.js
+++ Tooltip-pet/script.js
@@ -19,7 +19,8 @@ const handlers = {
 function hideTip (nodeTargetId) {
     let queryHelper = `.node:nth-child(` + nodeTargetId + `) > .node-info`;
     let nodeTargetedInfo = document.querySelector(queryHelper);
-    nodeTargetedInfo.style.opacity = `0`;     
+    nodeTargetedInfo.style.opacity = `0`;
+    nodeTargetedInfo.style.transition = `opacity ease-out .2s`;     
     }
 
 
@@ -28,6 +29,7 @@ function displayTip(nodeTargetId) {
     let nodeTargetedInfo = document.querySelector(queryHelper);
     // let displayStyle = getComputedStyle(nodeTargetedInfo).display;
     // console.log(displayStyle);
+    nodeTargetedInfo.style.transition = `opacity .8s`;
     nodeTargetedInfo.style.opacity = `1`;
 }
 
