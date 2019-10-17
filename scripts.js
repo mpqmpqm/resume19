3e59732adedd1fe6cd20008cd8d68be783f59dcf
mpqmpqm <53021641+mpqmpqm@users.noreply.github.com>
2019-10-16 21:50:04 -0400

    Good night, shittier page. Hello, new page!

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const colorButton = document.getElementById(`colorButton`)
+
+const nameDeco = document.getElementById(`name-deco`)
+// nameDeco.style.color = `blue`;
+
+function nameDecoColorAnimation (){
+    if (getComputedStyle(nameDeco).color == `rgb(255, 0, 0)`){
+        nameDeco.style.color = `blue`;
+  
+    }
+    else{
+        nameDeco.style.color = `red`;
+    }
+}
+
+setInterval(nameDecoColorAnimation, 802);
