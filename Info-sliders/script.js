e64445523e886c20e4cc3989b6bb0e46cd3a0128
mpqmpqm <53021641+mpqmpqm@users.noreply.github.com>
2019-10-27 16:33:09 -0400

    Add headers

--- Info-sliders/script.js
+++ Info-sliders/script.js
@@ -1,10 +1,10 @@
 const body = document.querySelector(`body`);
 const main = document.querySelector(`main`);
 const divs = Array.from(document.querySelectorAll(`main > div`));
-const labels = Array.from (document.querySelectorAll(`.label`));
-const texts = Array.from (document.querySelectorAll(`.text`));
+const labels = Array.from(document.querySelectorAll(`.label`));
+const texts = Array.from(document.querySelectorAll(`.text`));
 
-function setWindow () {
+function setWindow() {
     body.style.width = window.innerWidth + `px`
     body.style.height = window.innerHeight + `px`
     // divs.forEach(el => {
@@ -25,64 +25,72 @@ window.addEventListener(`resize`, setWindow)
 //     )
 // }
 
-function focus (event) {
+function focus(event) {
     let focused = event.target;
     divs.forEach((el, index) => {
-        if (el === focused){
-            window.requestAnimationFrame (() => {flexSetter(index);})
-            
-            }  
-        })
+        if (el === focused) {
+            window.requestAnimationFrame(() => {
+                flexSetter(index);
+            })
+
+        }
+    })
     labels.forEach((el, index) => {
-        if (el === focused){
-            window.requestAnimationFrame(() => {flexSetter(index);})
-        }}  
-    )
+        if (el === focused) {
+            window.requestAnimationFrame(() => {
+                flexSetter(index);
+            })
+        }
+    })
 }
 
 let lastFocused = 0;
 
-function flexSetter (index){
-    
-    for (let i = 0; i < 7; i++){
+function flexSetter(index) {
+
+    for (let i = 0; i < 7; i++) {
         if (i == index) {
             divs[i].style.flexBasis = `75%`;
             divs[i].style.flexGrow = `1`;
             divs[i].style.zIndex = `999`;
-            if (!window.matchMedia(`only screen and (orientation: portrait)`).matches){
-                if (index > lastFocused){
+            if (!window.matchMedia(`only screen and (orientation: portrait)`).matches) {
+                if (index > lastFocused) {
                     labels[i].style.transform = `rotateZ(-90deg)`;
                     labels[i].style.right = ``;
                     labels[i].style.left = `-1.6vw`;
+                } else {
+                    labels[i].style.transform = `rotateZ(90deg)`;
+                    labels[i].style.left = ``;
+                    labels[i].style.right = `-1.6vw`;
                 }
-                else {labels[i].style.transform = `rotateZ(90deg)`;
-                labels[i].style.left = ``;
-                labels[i].style.right = `-1.6vw`;}}
+            }
             texts[i].style.display = `block`;
-        }
-
-        else {
+        } else {
             divs[i].style.flexBasis = `calc(25% / 6)`;
             divs[i].style.flexGrow = `0`;
-            divs[i].style.zIndex = String (7 - Math.abs(i - index));
+            divs[i].style.zIndex = String(7 - Math.abs(i - index));
             texts[i].style.display = `none`
-            
-            if (!window.matchMedia(`only screen and (orientation: portrait)`).matches){
-            if (i < index){
-                if (getComputedStyle(labels[i]).transform == "matrix(6.12323e-17, 1, -1, 6.12323e-17, 0, 0)"){
-                    labels[i].style.opacity = `0`;
-                    setTimeout (()=>{labels[i].style.opacity = `1`;}, 100);
-                    labels[i].style.transform = `rotateZ(-90deg)`;}
-            }
 
-            else if (i > index){
-                if (getComputedStyle(labels[i]).transform == `matrix(6.12323e-17, -1, 1, 6.12323e-17, 0, 0)`)
-                    {labels[i].style.opacity = `0`;
-                    setTimeout (()=>{labels[i].style.opacity = `1`;}, 100);
-                    labels[i].style.transform = `rotateZ(90deg)`;}
+            if (!window.matchMedia(`only screen and (orientation: portrait)`).matches) {
+                if (i < index) {
+                    if (getComputedStyle(labels[i]).transform == "matrix(6.12323e-17, 1, -1, 6.12323e-17, 0, 0)") {
+                        labels[i].style.opacity = `0`;
+                        setTimeout(() => {
+                            labels[i].style.opacity = `1`;
+                        }, 100);
+                        labels[i].style.transform = `rotateZ(-90deg)`;
+                    }
+                } else if (i > index) {
+                    if (getComputedStyle(labels[i]).transform == `matrix(6.12323e-17, -1, 1, 6.12323e-17, 0, 0)`) {
+                        labels[i].style.opacity = `0`;
+                        setTimeout(() => {
+                            labels[i].style.opacity = `1`;
+                        }, 100);
+                        labels[i].style.transform = `rotateZ(90deg)`;
+                    }
+                }
             }
         }
-    }
     }
 
     lastFocused = index;
@@ -93,7 +101,7 @@ function flexSetter (index){
 //     k = 36;
 //     // for (let k = 16; k < 41; k++){
 //     for (let i = 0; i < 7; i++){
-        
+
 //         if (i == index){
 //             gridResult+= k +`% `
 //         }
