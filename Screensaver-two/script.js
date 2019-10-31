7b8485161cfc39245000cff19984abe41b949225
mpqmpqm <53021641+mpqmpqm@users.noreply.github.com>
2019-10-31 19:52:22 -0400

    Three.js, three.screensaver

--- Screensaver-two/script.js
+++ Screensaver-two/script.js
@@ -1,9 +1,17 @@
 const body = document.querySelector(`body`);
 // canvas.style.height = window.innerHeight +`px`
 // canvas.style.width = window.innerWidth +`px`; 
-let params = {width: window.innerWidth + `px`, height: window.innerHeight+ `px`}
+// let params = {width: window.innerWidth, height: window.innerHeight, 
+// // type: Two.Types.webgl
+// type: Two.Types.webgl
+// }
 
-const two = new Two(params).appendTo(body);
+const two = new Two ({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    type: Two.Types.webgl
+}).appendTo(body);
+// two.type = `WebGLRenderer`;
 
 // let line = two.makeLine (0, 0, window.innerWidth, window.innerHeight);
 // let lineTwo = two.makeLine ()
@@ -26,12 +34,12 @@ function addLine (x1, y1, x2, y2) {
 function drawLines () {
     let linesArray = [];
 
-    for (let i = 0; i <= 100; i+=.5) {
+    for (let i = 0; i <= 100; i+=4) {
         
         linesArray.push(addLine(0, percentToPixel(i, `y`), window.innerWidth, percentToPixel (100 - i, `y`)));
     }
 
-    for (let i = .5; i <= 99.5; i+=.5) {
+    for (let i = .5; i <= 99.5; i+=4) {
         
         linesArray.push(addLine(percentToPixel(100 - i, `x`), 0, percentToPixel(i, `x`), window.innerHeight));
     }
@@ -61,29 +69,29 @@ let line = 0;
 //     this.rotation = frameCount/16;
 // })
 
-two.bind (`update`, function (frameCount) {
+// two.bind (`update`, function (frameCount) {
 
-    if (switcher){
-        lines.children[line].opacity = 1;
-    }
+//     if (switcher){
+//         lines.children[line].opacity = 1;
+//     }
 
-    else {
-        lines.children[line].opacity = 0;
-    }
+//     else {
+//         lines.children[line].opacity = 0;
+//     }
 
-    line++;
+//     line++;
     
 
-    if (line == 400) {
-        line = 0;
-        switcher = !switcher;
-    }
+//     if (line == 400) {
+//         line = 0;
+//         switcher = !switcher;
+//     }
     
-}).play();
+// }).play();
 
 
 
 
 // lines.children[0].update();
 
-// two.update();
+two.update();
