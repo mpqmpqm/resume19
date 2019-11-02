e891148481d75c123685f1c843a315ab45d6a0c8
mpqmpqm <53021641+mpqmpqm@users.noreply.github.com>
2019-11-02 13:41:01 -0400

    cool pattern but not objective

--- three.screen-saver/script.js
+++ three.screen-saver/script.js
@@ -10,57 +10,172 @@ camera.position.set (0,0,100);
 camera.lookAt (0,0,0);
 const renderer = new THREE.WebGLRenderer();
 
+function setSize (){
 renderer.setSize (window.innerWidth*devicePixelRatio, window.innerHeight*devicePixelRatio);
 
+}
+
+setSize();
 document.body.appendChild(renderer.domElement);
 
+function resize() {
+    camera.aspect = window.innerWidth/window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize (
+    window.innerWidth*devicePixelRatio, window.innerHeight*devicePixelRatio);
+    renderer.render (scene, camera);
+}
+
+window.addEventListener(`resize`, resize);
 
 
-const material = new THREE.LineBasicMaterial ({color: 0xffffff});
 
-const geometry = new THREE.Geometry();
-geometry.vertices.push (new THREE.Vector3(-window.innerWidth,window.innerHeight,0));
-// geometry.vertices.push (new THREE.Vector3 (0 ,10, 0));
-geometry.vertices.push (new THREE.Vector3(window.innerWidth,-window.innerHeight,0));
 
-const line = new THREE.Line (geometry, material);
-line.linewidth = 2;
-const lineTwozie = new THREE.Line (geometry, material);
+// const geometry = new THREE.Geometry();
+// geometry.vertices.push (new THREE.Vector3(-window.innerWidth,window.innerHeight,0));
+// // geometry.vertices.push (new THREE.Vector3 (0 ,10, 0));
+// geometry.vertices.push (new THREE.Vector3(window.innerWidth,-window.innerHeight,0));
+
+// const line = new THREE.Line (geometry, material);
 // line.linewidth = 2;
+// const lineTwozie = new THREE.Line (geometry, material);
+// // line.linewidth = 2;
 
 let lines = [];
 
-for (let i = 0; i<=20; i++) {
-    let geometry = new THREE.Geometry();
-    geometry.vertices.push (new THREE.Vector3(-window.innerWidth,window.innerHeight - i* 100,0));
-    geometry.vertices.push (new THREE.Vector3(window.innerWidth,-window.innerHeight + i* 100,0));
-    lines.push (new THREE.Line (geometry, material));
+function percentToPixel (percent, axis) {
+    if (axis ==`x`) {
+        return Math.round((percent/100) * window.innerWidth);
+    }
+
+    else {
+     return Math.round((percent/100) * window.innerHeight);}
 }
+
+function makeLine (x1, y1, x2, y2){
+    let geometry = new THREE.Geometry();
+    geometry.vertices.push (new THREE.Vector3(x1, y1, 0));
+    geometry.vertices.push (new THREE.Vector3(x2, y2, 0));
+    let material = new THREE.LineBasicMaterial ({color: 0xffffff, transparent: true});
+    return new THREE.Line (geometry, material);
+}
+
+function drawLines () {
+    let linesArray = [];
+    
+    // linesArray.push (makeLine ((window.innerWidth/2), -(window.innerHeight/2), -window.innerWidth/2, window.innerHeight/2));
+
+    // linesArray.push (makeLine (-(window.innerWidth), window.innerHeight, window.innerWidth, -window.innerHeight));
+
+    for (let i = 0; i <=100; i+=2){
+        linesArray.push (makeLine (-window.innerWidth, window.innerHeight - percentToPixel(i, `y`), window.innerWidth, -window.innerHeight + percentToPixel(i, `y`)));
+    }
+
+    for (let i = 98; i >=0; i-=2) {
+        linesArray.push (makeLine (-window.innerWidth, -window.innerHeight + percentToPixel(i, `y`), window.innerWidth, window.innerHeight - percentToPixel(i, `y`)));
+    }
+
+    for (let i = 2; i <=98; i+=2) {
+        linesArray.push (makeLine(-window.innerWidth+percentToPixel(i, `x`), -window.innerHeight, window.innerWidth-percentToPixel(i, `x`), window.innerHeight));
+    }
+
+    for (let i = 100; i >=2; i-=2) {
+        linesArray.push (makeLine(window.innerWidth-percentToPixel(i, `x`), -window.innerHeight, -window.innerWidth+percentToPixel(i, `x`), window.innerHeight));
+    }
+
+    
+    // linesArray.push (makeLine (-(window.innerWidth), 0, window.innerWidth, 0));
+
+    // linesArray.push (makeLine (0, window.innerHeight, 0, -window.innerHeight));
+
+    // linesArray.push (makeLine (-(window.innerWidth), window.innerHeight-percentToPixel(50, `y`), window.innerWidth, -window.innerHeight + percentToPixel(50, `y`)));
+
+    // linesArray.push (makeLine (-(window.innerWidth), -window.innerHeight + percentToPixel(50, `y`), window.innerWidth, window.innerHeight - percentToPixel(50, `y`)));
+
+    // linesArray.push (makeLine (-(window.innerWidth), -window.innerHeight, window.innerWidth, window.innerHeight));
+    // linesArray.push (makeLine (-window.innerWidth, window.innerHeight - 100, window.innerWidth, -window.innerHeight + 100));
+
+    // linesArray.push (makeLine (-window.innerWidth, window.innerHeight - 300, window.innerWidth, -window.innerHeight + 300));
+
+    // linesArray.push (makeLine (-window.innerWidth, window.innerHeight - 600, window.innerWidth, -window.innerHeight + 600));
+
+    // linesArray.push (makeLine (-window.innerWidth, window.innerHeight - 800, window.innerWidth, -window.innerHeight + 800));
+
+    // linesArray.push (makeLine (-window.innerWidth, window.innerHeight - 700, window.innerWidth, -window.innerHeight + 700));
+    // linesArray.push (makeLine (-window.innerWidth, window.innerHeight - 913.75, window.innerWidth, -window.innerHeight + 913.75));
+    // for (let i = 0; i <= 100; i+=1) {
+    //     linesArray.push(makeLine(0, percentToPixel(i, `y`), window.innerWidth, percentToPixel (100 - i, `y`)));
+    // }
+
+    // for (let i = .5; i <= 99.5; i+=1) {
+        
+    //     linesArray.push(makeLine(percentToPixel(100 - i, `x`), 0, percentToPixel(i, `x`), window.innerHeight));
+    // }
+
+    return linesArray;
+
+}
+// lines.push(makeLine(0, 0, 400, 400));
 
 scene.background = new THREE.Color(0x000000);
 
 // let lines = [line, lineTwozie]
 
-// scene.add(lines[0],lines[1]);
+lines = drawLines();
+
 
 lines.forEach(line => {
-    
     scene.add(line);
+    // console.log(line);
 })
 // camera.position.z = 5;
 // cube.rotation.z = 1
 
+let opacitySwitch = true;
+let i = 0;
+let k = 1;
+
 function animate () {
     requestAnimationFrame(animate);
-    let i = 0;
-    lines.forEach(line => {
-        line.rotation.z -= 0.08
-        // setTimeout(() => }, i);
-        // i+=1;
-    })
+    
+    if (opacitySwitch){
+        lines[i].material.opacity = 0;
+        
+    }
+    
+    else {
+        lines[i].material.color =  {r: 0, g: 1, b:0};
+        lines[i].material.opacity = 1;
+        // lines[i].material.color =  {r: 1, g: 1, b: 1};
+    }
     
     // lineTwozie.rotation.z += 0.2;
+    // debugger;
     renderer.render (scene, camera);
+
+    i+=10;
+    if (i>=200){
+        i=0;
+        i+=k;
+        // k+=0;
+        if (k>=200){
+            k=0;
+            k++;
+        }
+        opacitySwitch = !opacitySwitch;
+    }
+    // debugger;
 }
 
+
+
+// renderer.render (scene, camera);
+
+
 animate();
+// animateK();
+// setTimeout(animate, 100);
+
+function test() {
+    renderer.render (scene, camera);
+}
