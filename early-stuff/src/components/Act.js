18a3b41f8491a5536b4d876978307ed2d636f400
mpqmpqm <53021641+mpqmpqm@users.noreply.github.com>
2019-12-23 17:13:51 -0500

    Semantics fix: h3s now h2s

--- early-stuff/src/components/Act.js
+++ early-stuff/src/components/Act.js
@@ -2,19 +2,19 @@ import React from 'react'
 
 function Act (props) {
     if (props.init) {
-        return <h3>Enter a musical act, get back their earliest releases.</h3>
+        return <h2>Enter a musical act, get back their earliest releases.</h2>
     }
 
     else if (props.loading) {
-        return <h3>Loading...</h3>
+        return <h2>Loading...</h2>
     }
 
     else if (!props.foundAct) {
-        return <h3>Not found. Please try again.</h3>
+        return <h2>Not found. Please try again.</h2>
     }
 
     else {
-        return <h3><a href = {props.link} target = '_blank' rel="noopener noreferrer">{props.name}</a></h3>
+        return <h2><a href = {props.link} target = '_blank' rel="noopener noreferrer">{props.name}</a></h2>
     }
 }
 
